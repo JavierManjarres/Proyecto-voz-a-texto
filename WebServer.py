cfc831539a225789a0058eee45b89e3c5ca56c6e
unknown <jmajar18@gmail.com>
2021-08-29 19:04:55 -0500

    version123

---
+++ WebServer.py
@@ -0,0 +1,74 @@
+from http.server import HTTPServer , BaseHTTPRequestHandler
+import os
+from flask import Flask, request, redirect, flash
+from werkzeug.utils import secure_filename
+import speech_recognition as sr
+
+app = Flask(__name__)
+UPLOAD_FOLDER = "./"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+# You have 50 free calls per day, after that you have to register somewhere
+# around here probably https://cloud.google.com/speech-to-text/
+GOOGLE_SPEECH_API_KEY = None
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    extra_line = ''
+    if request.method == "POST":
+        # Check if the post request has the file part.
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If user does not select file, browser also
+        # submit an empty part without filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file:
+            # Speech Recognition stuff.
+            recognizer = sr.Recognizer()
+            audio_file = sr.AudioFile(file)
+            with audio_file as source:
+                audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(
+                audio_data, key=GOOGLE_SPEECH_API_KEY, language="ru-RU"
+            )
+            extra_line = f'Your text: "{text}"'
+
+            # Saving the file.
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(filepath)
+            extra_line += f"<br>File saved to {filepath}"
+
+    return f"""
+    <!doctype html>
+    <title>Upload new File</title>
+    {extra_line}
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <p/>
+      <input type=submit value=Upload>
+    </form>
+    """
+
+
+class helloHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write('hola mundo'.encode())
+
+def main():
+    PORT = 8000
+    server = HTTPServer(('', PORT), helloHandler)
+    print('Server running on port %s' % PORT)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
